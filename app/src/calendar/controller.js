2c742ec319359dd77f319fabfdb1f9ebdb8506e6
C1296135 Eduardo Soares Sampaio <eduardo.ssampaio@outlook.com>
2019-10-02 15:20:56 -0300

    ajustando locale e ajustes visuais

--- app/src/calendar/controller.js
+++ app/src/calendar/controller.js
@@ -1,12 +1,15 @@
 
 var moment = require('moment');
+moment.locale('pt-BR');
+
 var app = new Vue({
     el: '#app',
     mounted:function (){
-console.log('monted: ', this);
+
         this.refresh();
     },
     data: {
+
         atualDate: moment(),
         message: 'Hello Vue!',
         monthName :"",
@@ -23,34 +26,34 @@ console.log('monted: ', this);
             this.refresh();
         },
 
-        refresh : function() {
-            this.monthName =  this.atualDate.format("MMMM")
-            var currentMonth = this.atualDate.get('month');
-            // console.log('currentMonth: ',currentMonth);
-            var firstDayOfMonth = moment(this.atualDate).startOf('month'); //this.atualDate.set(1, 'day');
-            var lastDayOfMonth = moment(this.atualDate).endOf('month');
+        firstDayOfMonth: function() {
+            var firstDayOfMonth = moment(this.atualDate).startOf('month');
             var initialDay = firstDayOfMonth.day("Sunday"); //first sunday of week
-            var finalDay = lastDayOfMonth.day("Saturday"); //last sunday of week
             initialDay = initialDay.add(-1,'day');
-            console.log(initialDay.format('DD MM YYYY') + ': '+  initialDay.dayOfYear());
-            console.log(finalDay.format('DD MM YYYY') + ': '+ finalDay.dayOfYear());
+            return initialDay;
+        },
 
+        refresh : function() {
+            // var currentDate;
+            this.monthName =  this.atualDate.format("MMMM")
+            this.year =  this.atualDate.format("YYYY")
+            var currentMonth = this.atualDate.get('month');
+            var initialDay = this.firstDayOfMonth();
             this.weeks = [];
 
-            //while(initialDay.dayOfYear() < finalDay.dayOfYear()){
             for(var i=0; i< 5; i++){ //5 weeks
                 var week = [];
                 for(var j=0; j< 7; j++){
                     initialDay =initialDay.add(1, 'day');
 
                     week.push({
+                        today: initialDay.isSame(new Date(), "day"),
                         day: initialDay.get('date'),
                         otherMonth: initialDay.get('month') !== currentMonth
                     });
                 }
                 this.weeks.push(week);
             }
-            // console.log(this.weeks);
         }
     }
 
--- main.js
+++ main.js
@@ -1,21 +1,22 @@
-const { menubar } = require('menubar');
+var moment = require('moment');
+const {
+    menubar
+} = require('menubar');
 const rootPath = require('electron-root-path').rootPath;
 
 const mb = menubar({
-  preloadWindow: true,
-  webPreferences : {
-    nodeIntegration: true,
-  },
-  browserWindow: {
-    width: 320,
-    height: 380
-  },
-  index:'file:///' +rootPath + '/app/src/calendar/calendar.html'
+    preloadWindow: true,
+    webPreferences: {
+        nodeIntegration: true,
+    },
+    browserWindow: {
+        width: 320,
+        height: 380
+    },
+    index: 'file:///' + rootPath + '/app/src/calendar/calendar.html'
 });
 
 mb.on('ready', () => {
-  console.log('app is ready');
-  // console.log(mb.window);
-  // mb.window.webContents.openDevTools()
-  // your app code here
+    mb.window.setResizable(false);
+    console.log('app is ready');
 });
